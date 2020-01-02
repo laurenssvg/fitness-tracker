18637f47b1a30956c2ea1cf222f322cad35d7a81
laurenssvg <l.vangellecum@gmail.com>
2020-01-02 11:14:53 +0100

    training componenten gemaakt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,15 @@ import { MaterialModule } from "./material.module";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { LoginComponent } from "./auth/login/login.component";
-import { TrainingComponent } from "./training/training/training.component";
+import { TrainingComponent } from "./training/training.component";
 import { CurrentTrainingComponent } from "./training/current-training/current-training.component";
 import { NewTrainingComponent } from "./training/new-training/new-training.component";
 import { WelcomeComponent } from "./welcome/welcome.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { HeaderComponent } from "./navigation/header/header.component";
+import { SidenavListComponent } from "./navigation/sidenav-list/sidenav-list.component";
+import { PastTrainingsComponent } from "./training/past-trainings/past-trainings.component";
+import { StopTrainingComponent } from "./training/current-training/stop-training.component";
 
 @NgModule({
   declarations: [
@@ -22,7 +26,11 @@ import { AppRoutingModule } from "./app-routing.module";
     TrainingComponent,
     CurrentTrainingComponent,
     NewTrainingComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    PastTrainingsComponent,
+    StopTrainingComponent
   ],
   imports: [
     BrowserModule,
@@ -34,6 +42,7 @@ import { AppRoutingModule } from "./app-routing.module";
     ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [StopTrainingComponent]
 })
 export class AppModule {}
